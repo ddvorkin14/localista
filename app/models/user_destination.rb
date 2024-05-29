fff90e08a52e379365215101df08c548debf1f71
Daniel Dvorkin <dvorkin212@gmail.com>
2024-05-29 00:27:31 -0400

    added user destinations, fixed up the form so the user object state is passed correclty between steps

---
+++ app/models/user_destination.rb
@@ -0,0 +1,4 @@
+class UserDestination < ApplicationRecord
+  belongs_to :user
+  validates :name, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_29_025213) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_29_042309) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_29_025213) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "user_destinations", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "name", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
